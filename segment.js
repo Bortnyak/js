dee6f83c872727b8a6825e7217f116569ea1b297
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-10-31 18:22:34 +0200

    fixed segment.js

--- segment.js
+++ segment.js
@@ -5,36 +5,20 @@ import { cons, car, cdr } from 'hexlet-pairs';
 // BEGIN (write your solution here)
 import * as points from 'hexlet-points';
 
-export const makeSegment = (point1, point2) => {
-  const segment = makePoint(point1, point2);
-  return segment;
-}
-
-export const startSegment = (segment) => {
-  const startPoint = car(segment);
-  return startPoint;
-}
-
-export const endSegment = (segment) => {
-  const endPoint = cdr(segment);
-  return endPoint;
-}
-
-export const segmentToString = (segment) => {
-  return `[${pointToString(car(segment))}, ${pointToString(cdr(segment))}]`;
-}
-
+export const makeSegment = (point1, point2) => cons(point1, point2);
+export const startSegment = segment => car(segment);
+export const endSegment = segment => cdr(segment);
+export const segmentToString = segment => `[${pointToString(car(segment))}, ` + `${pointToString(cdr(segment))}]`;
 export const midpointSegment = (segment) => {
-  const x1 = car(car(segment));
-  const x2 = car(cdr(segment));
-  const y1 = cdr(car(segment));
-  const y2 = cdr(cdr(segment));
-
+  const x1 = car(startSegment(segment));
+  const y1 = cdr(startSegment(segment));
+  const x2 = car(endSegment(segment));
+  const y2 = cdr(endSegment(segment));
+  
   const x = (x1 + x2) / 2;
   const y = (y1 + y2) / 2;
-
-  const point = makePoint(x,y);
-
-  return point;
-}
+  
+  const midPoint = cons(x, y);
+  return midPoint;
+};
 // END
