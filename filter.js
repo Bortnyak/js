a53b2069e53c4fb96020064538c15bdadbd4f220
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-11-01 19:31:07 +0200

    add filter func, quotes func

---
+++ filter.js
@@ -0,0 +1,18 @@
+export const filter = (func, elements) => {
+    const iter = (items, acc) => {
+      if (isEmpty(items)) {
+        return reverse(acc);
+      }
+      const item = head(items);
+      const newAcc = func(item) ? cons(item, acc) : acc;
+      return iter(tail(items), newAcc);
+    };
+  
+    return iter(elements, l());
+  };
+  
+  export const quotes = (elements) => {
+    const filtered = filter(element => is('blockquote', element), elements);
+    const result = map(value, filtered);
+    return result;
+  };
